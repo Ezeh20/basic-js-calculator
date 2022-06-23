7f37c00900033d908df7591d6fbe617daa145a6c
Ezeh20 <buchimmaduabuchukwu@gmail.com>
2022-06-23 19:21:13 +0100

    changes

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
+let operation=Number(prompt("Welcome \nSelect the operation you wish to perform :\n \nenter 1 for addition\nenter 2 for subtraction\nenter 3 for multiplcation\nenter 4  for division"))
 let firstNumber=Number(prompt("Enter first number \n"))
 let secondNumber=Number(prompt("Enter second number \n"))
-let operation=Number(prompt("Select the operation you wish to perform :\n \nenter 1 for addition\nenter 2 for subtraction\nenter 3 for multiplcation\nenter 4  for division"))
+let answer="";
 
 
 if(operation === 1){
     answer=firstNumber + secondNumber
-    alert(answer)
 }
 else if(operation === 2){
     answer=firstNumber - secondNumber
-    alert(answer)
 }
 else if(operation === 3){
     answer=firstNumber * secondNumber
-    alert(answer)
+    
 }
 else if(operation === 4){
     answer=firstNumber / secondNumber
-    alert(answer)
+   
 }else{
     alert("enter a valid option")
 }
+alert(answer)
 
 
